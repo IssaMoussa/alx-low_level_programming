20f7e7f13d820d44d5d7a9c78047001ca8b2f564
IssaMoussa <ssmssdiop@gmail.com>
2022-09-19 06:49:46 -0700

    prin_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ *
+ * main - entry
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int number;
+	int d;
+
+	for (number = '0'; number < '9'; number++)
+	{
+		for (d = number+1; d <= '9'; d++)
+		{
+			putchar(number);
+			putchar(d);
+			if (d != '8')
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	return (0);
+}
