842049b81c7a1d552730c6f2cf693f658e6a63ad
IssaMoussa <ssmssdiop@gmail.com>
2022-09-19 05:53:05 -0700

    4 alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		if (low != 'e' or low != 'q')
+			putchar(low);
+	putchar('\n');
+
+	return (0);
+}
