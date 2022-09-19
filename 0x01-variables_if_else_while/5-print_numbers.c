3eeaea43f8590433e4d37ab4707d670b14f79493
IssaMoussa <ssmssdiop@gmail.com>
2022-09-19 06:00:26 -0700

    5-number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - entry
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+		printf("%d", number);
+	printf("\n");
+
+	return (0);
+}
