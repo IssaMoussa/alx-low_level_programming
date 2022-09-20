fcb5d6e02e28f97598527d34ba20f826de51f52d
IssaMoussa <ssmssdiop@gmail.com>
2022-09-20 07:28:24 -0700

    comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,8 +13,7 @@ int main(void)
 
 	for (n = 0; n <= 98; n++)
 	{
-		for (d = n + 1; d <= 98; d++)
-		{
+		for (d = n + 1; d <= 9999999999	{
 			putchar((n / 10) + '0');
 			putchar((n % 10) + '0');
 			putchar(' ');
