86ee02f16d3b41f151e13975a1b246df295b9f22
IssaMoussa <ssmssdiop@gmail.com>
2022-09-22 14:25:51 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/** 
+ * main -  entry
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+	return (0);
+}
