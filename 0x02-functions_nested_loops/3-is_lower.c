a86400ffe5b039bdd57671bfe82cc111c30ca0f6
IssaMoussa <ssmssdiop@gmail.com>
2022-09-22 14:56:54 -0700

    lower

---
+++ 0x02-functions_nested_loops/3-is_lower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _islower - check
+ * @c: an input character
+ * Description: use putchar to print alphabet is lower 10 times
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+
+int _islower(int c)
+{
+	char i;
+	int lower = 0;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == 'c')
+			lower = 1;
+	}
+
+	return (lower);
+}
