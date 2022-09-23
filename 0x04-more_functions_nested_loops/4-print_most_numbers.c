a365bfef1fc8b7c6dff9336a0ce718f4f847beae
IssaMoussa <ssmssdiop@gmail.com>
2022-09-23 10:03:01 -0700

    print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,6 +3,7 @@
 /**
  * print_most_numbers - check
  * do not pritnt 2 and 4
+ * followed by a new line
  * Return: nothing
  */
 
@@ -14,6 +15,6 @@ void print_most_numbers(void)
 	{
 		if (i != '2' && i != '4')
 			_putchar(i);
-		putchar('\n');
 	}
+	_putchar('\n');
 }
