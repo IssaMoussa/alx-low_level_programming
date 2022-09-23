66caac926b1f0cf9c0da04b5fb52a88bfbada6cb
IssaMoussa <ssmssdiop@gmail.com>
2022-09-23 07:33:50 -0700

    9 11 exos

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - check
+ * @a: input integer 
+ * @b: input integer
+ * Description: fucont add two integer 
+ * Return: a add to b
+ */
+
+int add(int a, inb)
+{
+	return (a +b);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - check
+ * @n: an integer input
+ * Description: print all natural number from 0 to 98
+ * Return: nothing
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+}
