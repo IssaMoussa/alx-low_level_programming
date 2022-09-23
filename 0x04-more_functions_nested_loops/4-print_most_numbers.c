2974f632f5d56784ae45ee66f2677ad21fc7355c
IssaMoussa <ssmssdiop@gmail.com>
2022-09-23 09:56:32 -0700

    4 5 exos

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - check
+ * do not pritnt 2 and 4
+ * Return: nothing
+ */
+
+void print_most_numbers(void)
+{
+	int i = '0';
+
+	for (; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+			_putchar(i);
+		putchar('\n');
+	}
+}
