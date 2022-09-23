03242ebce28855fea482910d81dda588c8034736
IssaMoussa <ssmssdiop@gmail.com>
2022-09-23 10:23:51 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - draws straigth line
+ * followed by a new line
+ * @n: input integer
+ * Return: nothing
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+	
+	if (n > 0)
+	{
+		for (; i < n; i++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
