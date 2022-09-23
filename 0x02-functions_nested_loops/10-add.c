850a03dbdba694d9251aca3601dceeb033f0b48c
IssaMoussa <ssmssdiop@gmail.com>
2022-09-23 07:40:35 -0700

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,13 +2,13 @@
 
 /**
  * add - check
- * @a: input integer 
+ * @a: input integer
  * @b: input integer
- * Description: fucont add two integer 
+ * Description: fucont add two integer
  * Return: a add to b
  */
 
 int add(int a, int b)
 {
-	return (a +b);
+	return (a + b);
 }
