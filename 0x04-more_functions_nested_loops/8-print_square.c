66eff7fc354ea3b053832468b3a2fbd74a32c3e8
IssaMoussa <ssmssdiop@gmail.com>
2022-09-24 14:53:44 -0700

    correction

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,13 +9,13 @@
 
 void print_square(int n)
 {
-	int i = 0, j = 0;
+	int i, j;
 
 	if (n > 0)
 	{
-		for (; i < n; i++)
+		for (i = 0; i < n; i++)
 		{
-			for (; j < n; j++)
+			for (j = 0; j < n; j++)
 				_putchar('#');
 			_putchar('\n');
 		}
